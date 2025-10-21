d3ce03d4976d35b2a0f24f709158e5df98a89387
Razzer101 <Rasmus@anders-net.dk>
2025-10-21 13:53:08 +0200

    NewsBegun

---
+++ src/components/News.jsx
@@ -0,0 +1,79 @@
+import { useEffect, useRef, useState } from "react"
+import gsap from "gsap"
+
+export default function News({ category, title }){
+
+    const [newsData, setNewsData] = useState(null)
+    const [isOpen, setIsOpen] = useState(false)
+    const newsDisplay = useRef()
+    const arrow = useRef()
+
+    useEffect(()=>{
+        const API_KEY = import.meta.env.VITE_API_KEY
+
+        const fetchData = async () => {
+            const url = new URL(`https://api.nytimes.com/svc/topstories/v2/${category}.json`)
+            url.searchParams.set("api-key", API_KEY)
+
+            const response = await fetch(url)
+            const data = await response.json()
+                setNewsData(data.results.slice(0, 5))
+                console.log(data.results.slice(0, 5)) 
+
+        }
+
+        fetchData()
+    },[])
+    
+    const handleOpenClick = () => {
+        gsap.to(newsDisplay.current, {
+            height: "auto",
+            paddingBottom: 20,
+        })
+        gsap.to(arrow.current, {
+            rotateZ: 90
+        })
+
+        setIsOpen(true)
+    }
+
+        const handleCloseClick = () => {
+        gsap.to(newsDisplay.current, {
+            height: 0,
+            paddingBottom: 0,
+        })
+        gsap.to(arrow.current, {
+            rotateZ: 0
+        })
+
+        setIsOpen(false)
+    }
+
+    return(
+        
+        <div className="news-category">
+            <button onClick={() => isOpen ? handleCloseClick() : handleOpenClick()} className="news-category__btn">
+                <img src="newsifyLogo.png" alt="Newsify logo" />
+                <h2>{title}</h2>
+                <div ref={arrow}>&#8250;</div>
+            </button>
+            <div ref={newsDisplay} className="news-display">
+                { 
+                newsData !== null &&
+                    newsData.map((elm) => {
+                        return (
+                            <a key={elm.uri} className="news-box" href={elm.url}>
+                                <img className="news-box__img" src="https://placehold.co/200?text=No%20Img" alt={`${elm.title.slice(0, 30)}...`} />
+                                <article className="news-box__article">
+                                    <h3>{elm.title.slice(0, 25)}...</h3>
+                                    <p>{elm.abstract.slice(0, 60)}...</p>
+                                </article>
+                            </a>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+    
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,9 @@ import Login from "../components/Login"
 import Header from "../components/Header"
 import Searchbar from "../components/Searchbar"
 import Nav from "../components/Nav"
+import News from "../components/News"
 
 export default function Home(){
-
 
     return(
         <>
@@ -15,7 +15,11 @@ export default function Home(){
         <Login/>
         <Header/>
         <Searchbar/>
-        <section className="home-section"></section>
+        <section className="news-section">
+            <News category="health" title="health"/>
+            {/* <News category="sports" title="Sport"/> */}
+            <News category="travel" title="Travel"/>
+        </section>
         <Nav/>
         </>
     )
